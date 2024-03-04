5d66aa7f997cef077dd35f64bf598a351e010ab8
Guang Yang <gyang1@cs.washington.edu>
2024-03-04 13:42:45 -0800

    Adjusting filtering threshold in infer.py Add dashed line visualization in infer.py

--- objectdetection/infer.py
+++ objectdetection/infer.py
@@ -1,4 +1,4 @@
-import os
+import os, math
 import torch, torchvision
 import scipy
 import numpy as np
@@ -16,6 +16,89 @@ PATCH_SIZE=1216
 N_CLASSES=128
 MARGIN=128
 STEP_SIZE=PATCH_SIZE-2*MARGIN
+
+class DashedImageDraw(ImageDraw.ImageDraw):
+
+    def thick_line(self, xy, direction, fill=None, width=0):
+        #xy – Sequence of 2-tuples like [(x, y), (x, y), ...]
+        #direction – Sequence of 2-tuples like [(x, y), (x, y), ...]
+        if xy[0] != xy[1]:
+            self.line(xy, fill = fill, width = width)
+        else:
+            x1, y1 = xy[0]            
+            dx1, dy1 = direction[0]
+            dx2, dy2 = direction[1]
+            if dy2 - dy1 < 0:
+                x1 -= 1
+            if dx2 - dx1 < 0:
+                y1 -= 1
+            if dy2 - dy1 != 0:
+                if dx2 - dx1 != 0:
+                    k = - (dx2 - dx1)/(dy2 - dy1)
+                    a = 1/math.sqrt(1 + k**2)
+                    b = (width*a - 1) /2
+                else:
+                    k = 0
+                    b = (width - 1)/2
+                x3 = x1 - math.floor(b)
+                y3 = y1 - int(k*b)
+                x4 = x1 + math.ceil(b)
+                y4 = y1 + int(k*b)
+            else:
+                x3 = x1
+                y3 = y1 - math.floor((width - 1)/2)
+                x4 = x1
+                y4 = y1 + math.ceil((width - 1)/2)
+            self.line([(x3, y3), (x4, y4)], fill = fill, width = 1)
+        return   
+        
+    def dashed_line(self, xy, dash=(2,2), fill=None, width=0):
+        #xy – Sequence of 2-tuples like [(x, y), (x, y), ...]
+        for i in range(len(xy) - 1):
+            x1, y1 = xy[i]
+            x2, y2 = xy[i + 1]
+            x_length = x2 - x1
+            y_length = y2 - y1
+            length = math.sqrt(x_length**2 + y_length**2)
+            dash_enabled = True
+            postion = 0
+            while postion <= length:
+                for dash_step in dash:
+                    if postion > length:
+                        break
+                    if dash_enabled:
+                        start = postion/length
+                        end = min((postion + dash_step - 1) / length, 1)
+                        self.thick_line([(round(x1 + start*x_length),
+                                          round(y1 + start*y_length)),
+                                         (round(x1 + end*x_length),
+                                          round(y1 + end*y_length))],
+                                        xy, fill, width)
+                    dash_enabled = not dash_enabled
+                    postion += dash_step
+        return
+
+    def dashed_rectangle(self, xy, dash=(2,2), outline=None, width=0):
+        #xy - Sequence of [(x1, y1), (x2, y2)] where (x1, y1) is top left corner and (x2, y2) is bottom right corner
+        x1, y1 = xy[0]
+        x2, y2 = xy[1]
+        halfwidth1 = math.floor((width - 1)/2)
+        halfwidth2 = math.ceil((width - 1)/2)
+        min_dash_gap = min(dash[1::2])
+        end_change1 = halfwidth1 + min_dash_gap + 1
+        end_change2 = halfwidth2 + min_dash_gap + 1
+        odd_width_change = (width - 1)%2        
+        self.dashed_line([(x1 - halfwidth1, y1), (x2 - end_change1, y1)],
+                         dash, outline, width)       
+        self.dashed_line([(x2, y1 - halfwidth1), (x2, y2 - end_change1)],
+                         dash, outline, width)
+        self.dashed_line([(x2 + halfwidth2, y2 + odd_width_change),
+                          (x1 + end_change2, y2 + odd_width_change)],
+                         dash, outline, width)
+        self.dashed_line([(x1 + odd_width_change, y2 + halfwidth2),
+                          (x1 + odd_width_change, y1 + end_change2)],
+                         dash, outline, width)
+        return
 
 def bbox_iop(box1, box2, eps=1e-7):
     b1_x1, b1_y1, b1_x2, b1_y2 = box1
@@ -84,9 +167,7 @@ if __name__ == "__main__":
         def remove_truncated(plist, true_box, prob):
             to_remove = None
             for idx, (box_pre, prob_pre) in enumerate(plist):
-                if np.dot(prob.data, prob_pre.data) < 0.10:
-                    continue
-                if bbox_iop(box_pre, true_box) > 0.80:
+                if bbox_iop(box_pre, true_box)*np.dot(prob.data, prob_pre.data) > 0.20:
                     to_remove = idx
                     break
             if to_remove:
@@ -115,13 +196,13 @@ if __name__ == "__main__":
         for img_name, preds_dict in tqdm(imgname2preds.items(), desc="saving visualization files..."):
             img_n = img_name.replace('ideal', 'symbol')
             img = Image.open(os.path.join(args.images, img_n)).convert("RGB")
-            draw = ImageDraw.Draw(img)
+            draw = DashedImageDraw(img)
             for (x_id, y_id), preds_list in preds_dict.items():
                 for box, prob in preds_list:
                     draw.rectangle(tuple(box), outline=color_palette(int(prob.top1)), width=3)
                 if args.grids:
                     draw.rectangle((x_id*STEP_SIZE, y_id*STEP_SIZE, (x_id+1)*STEP_SIZE, (y_id+1)*STEP_SIZE), outline=(255, 255, 255), width=2)
-                    draw.rectangle((x_id*STEP_SIZE+MARGIN, y_id*STEP_SIZE+MARGIN, (x_id+1)*STEP_SIZE-MARGIN, (y_id+1)*STEP_SIZE-MARGIN), outline=(255, 255, 255), width=2)
+                    draw.dashed_rectangle(((x_id*STEP_SIZE-MARGIN, y_id*STEP_SIZE-MARGIN), ((x_id+1)*STEP_SIZE+MARGIN, (y_id+1)*STEP_SIZE+MARGIN)), dash=(10, 10), outline=(255, 255, 255), width=2)
             img.save(os.path.join(args.save_dir, 'data', 'visualization', img_n))
     
     # Load classes
